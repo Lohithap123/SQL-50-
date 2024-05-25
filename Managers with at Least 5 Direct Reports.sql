0772c055cca62586f22c40e0a74a39255d43f748
Lohitha Lakshmi Petlu <155876034+Lohithap123@users.noreply.github.com>
2024-05-25 19:11:15 +0530

    Managers with at Least 5 Direct Reports.sql

---
+++ Managers with at Least 5 Direct Reports.sql
@@ -0,0 +1,12 @@
+select 
+    name
+from 
+    employee
+where
+    id in ( select 
+                managerid
+            from 
+                employee
+            group by 
+                managerid
+            having count(*)>=5);
